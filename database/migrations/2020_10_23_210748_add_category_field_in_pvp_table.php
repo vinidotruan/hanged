6cdb3aa4095fea5d8e3f6013fd5558c813b8c8c0
Ruan Vinícius Silva da Silva <ruan.vinicius@dbccompany.com.br>
2020-10-23 18:19:05 -0300

    - save

--- database/migrations/2020_10_23_210748_add_category_field_in_pvp_table.php
+++ database/migrations/2020_10_23_210748_add_category_field_in_pvp_table.php
@@ -14,7 +14,7 @@ class AddCategoryFieldInPvpTable extends Migration
     public function up()
     {
         Schema::table('pvp_games', function (Blueprint $table) {
-            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('category_id')->default(1);
             $table->foreign('category_id')->references('id')->on('categories');
         });
     }
