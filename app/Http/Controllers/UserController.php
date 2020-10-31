e56acbc12fbf6215d486639a7ae2916721e25522
Ruan Vinícius Silva da Silva <ruan.vinicius@dbccompany.com.br>
2020-10-31 15:06:31 -0300

    - change responde to login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -79,6 +79,6 @@ class UserController extends Controller
     public function login(Request $request ) {
         $user = User::where('email', $request->emai)->get();
         $response = [ 'user' => $user, 'logged' => true, 'admin' => ($request->email == 'admin@admin.com')];
-        return response()->json($user);
+        return response()->json($response);
     }
 }
