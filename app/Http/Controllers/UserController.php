9e8dfdf54ea08e2621011911e95389710f5ca9d0
Ruan Vinícius Silva da Silva <ruan.vinicius@dbccompany.com.br>
2020-10-31 15:23:19 -0300

    - add debug request

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -77,6 +77,7 @@ class UserController extends Controller
     }
 
     public function login(Request $request ) {
+        dd($request);
         $user = User::where('email', $request->emai)->get();
         $response = [ 'user' => $user, 'logged' => true, 'admin' => ($request->email == 'admin@admin.com')];
         return response()->json($response);
